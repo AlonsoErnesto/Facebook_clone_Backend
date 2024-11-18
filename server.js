d95ee10fdf3774bf4837f7af69776359cb2f5b3b
AlonsoErnesto <ernesto134alonso123@gmail.com>
2024-11-18 18:57:09 -0500

    fix(bug):token

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*", methods: "GET, POST, PUT, DELETE" }));
 app.use(
   fileUpload({
     useTempFiles: true,
