f4b45ed230cbdf190708d564691343acfd0de8ce
AlonsoErnesto <ernesto134alonso123@gmail.com>
2024-11-19 09:58:52 -0500

    fix(cors):config cors

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.use(express.json());
 app.use(
   cors({
     origin: process.env.URL_FRONT_END,
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
   })
 );
 app.use(
   fileUpload({
-    useTempFiles: true,
+    useTempFiles: true
   })
 );
 //routes
@@ -26,7 +26,7 @@ readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 //database
 mongoose
   .connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
+    useNewUrlParser: true
   })
   .then(() => console.log("database connected successfully"))
   .catch((err) => console.log("error connecting to mongodb", err));
