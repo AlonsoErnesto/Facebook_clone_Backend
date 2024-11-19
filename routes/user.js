3a9923b25346bc0b892612aedcd56f2ab90cfdad
AlonsoErnesto <ernesto134alonso123@gmail.com>
2024-11-18 19:34:49 -0500

    feat(test):endpoint

--- routes/user.js
+++ routes/user.js
@@ -33,6 +33,11 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/activate", authUser, activateAccount);
 router.post("/login", login);
+router.get("/example", (req, res) => {
+  res.write("Este endpoint y el servidor funcionan correctamente.");
+  res.end();
+});
+
 router.post("/sendVerification", authUser, sendVerification);
 router.post("/findUser", findUser);
 router.post("/sendResetPasswordCode", sendResetPasswordCode);
